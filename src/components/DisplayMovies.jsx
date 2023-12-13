6b31ae29925a6c231a2529bba3c5fa90119bb6ff
Prakriti <prakritiadhikari2017@gmail.com>
2023-12-12 20:40:18 -0600

    edit displaymovies css

--- src/components/DisplayMovies.jsx
+++ src/components/DisplayMovies.jsx
@@ -1,29 +1,32 @@
-import './DisplayMovies.css'
-import formatDate from '../util/formatDate';
+import "./DisplayMovies.css";
+import formatDate from "../util/formatDate";
 
 export default function DisplayMovies({ moviesInfo }) {
   return (
-    <table className="movies-table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Release Date</th>
-          <th>Rating</th>
-          <th>Genre</th>
-          <th>Studio Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {moviesInfo.map((movie) => (
-          <tr key={movie.movieId}>
-            <td>{movie.movieTitle}</td>
-            <td>{formatDate(movie.releaseDate)}</td>
-            <td>{movie.movieRating}</td>
-            <td>{movie.genre}</td>
-            <td>{movie.studioEmail}</td>
+    <>
+    <hr></hr>
+      <table className="movies-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+            <th>Genre</th>
+            <th>Studio Email</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {moviesInfo.map((movie) => (
+            <tr key={movie.movieId}>
+              <td>{movie.movieTitle}</td>
+              <td>{formatDate(movie.releaseDate)}</td>
+              <td>{movie.movieRating}</td>
+              <td>{movie.genre}</td>
+              <td>{movie.studioEmail}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
