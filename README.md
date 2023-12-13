5f769aaffeee8dd0c2259e4b41e1afa3bc487961
Prakriti <prakritiadhikari2017@gmail.com>
2023-12-12 19:14:07 -0600

    update tests

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 This application allows users to enter information about their favorite movies.
 
+## Technologies Used
+* ReactJS
+
 ## Building and running:
 
-#### `npm install`: Install dependencies
-#### `npm run dev`: Start dev server
+#### `npm install`:Install dependencies
+#### `npm run dev`:Start dev server
 
 ## Testing:
 
--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { render, fireEvent } from "@testing-library/react"
 import App from "../App"
 
-describe('App component',()=>{
+describe('DisplayMovies component',()=>{
 
     test("Renders the Movie Title", () => {
         const {getByLabelText}=render(<App />)
@@ -13,42 +13,37 @@ describe('App component',()=>{
         expect(title.value).toBe('Barbie')
 
     })
-    test("Renders the Release Date", () => {
-        const {getByLabelText}=render(<App />)
-        const releaseDate = getByLabelText('Release Date:')
-    
-        fireEvent.change(releaseDate, {target: {value: '2023-10-09'}})
-        
-        expect(releaseDate.value).toBe('2023-10-09')
-
-    })
-    test("Renders the Movie Rating", () => {
-        const {getByLabelText}=render(<App />)
-        const rating = getByLabelText('Movie Rating:')
-    
-        fireEvent.change(rating, {target: {value: 3.6}})
-        
-        expect(rating.value).toBe(3.6)
-
-    })
-    test("Renders the Genre", () => {
-        const {getByLabelText}=render(<App />)
-        const genre = getByLabelText('Genre:')
-    
-        fireEvent.change(genre, {target: {value: 'Action'}})
-        
-        expect(genre.value).toBe('Action')
-
-    })
-    test("Renders the Studio Email", () => {
-        const {getByLabelText}=render(<App />)
-        const studioEmail = getByLabelText('Studio Email:')
-    
-        fireEvent.change(studioEmail, {target: {value: 'Action'}})
-        
-        expect(studioEmail.value).toBe('Action')
-
-    })
 
 })
+
+describe('MovieForm component',()=>{
+    test('displays error message when movieTitle is invalid',()=>{
+
+        const {getByLabelText, getByText}=render(<App />)
+        const title = getByLabelText('Movie Title:')
+    
+        fireEvent.change(title, {target: {value: ''}})
+        fireEvent.blur(title)
+        
+        const errorElement = getByText('Please enter a Movie Title',{exact:false})
+        expect(errorElement).toBeInTheDocument()
+
+    })
+
+    test('removes error message when movieTitle is valid',()=>{
+
+        const {getByLabelText, queryByText}=render(<App />)
+        const title = getByLabelText('Movie Title:')
+    
+        fireEvent.change(title, {target: {value: 'Spiderman'}})
+        fireEvent.blur(title)
+        
+        const errorElement = queryByText('Please enter a Movie Title',{exact:false})
+        expect(errorElement).toBeNull()
+
+    })
+}
+)
+
+
 
